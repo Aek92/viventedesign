a91ac96e7b9bb8ac7855d153e80232ce0b30103d
Aek92 <aek92.ak@gmail.com>
2023-08-27 15:11:38 +0200

    mobile css

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-import TopImage from "../../assets/top/top.jpg";
 import "./header.styles.scss";
 
 const Header = () => {
